11348a9347e4e864fbaec4f2ed2cea20201bb754
3000y <821923589@qq.com>
2026-08-22 09:37:26 +0800

    macOS 补齐应用菜单栏与系统快捷键

--- apps/desktop/internal/chrome/chrome_darwin.go
+++ apps/desktop/internal/chrome/chrome_darwin.go
@@ -16,6 +16,63 @@ void styleWindow(void *window) {
 	[nsWindow setTitleVisibility:NSWindowTitleHidden];
 	[nsWindow setMovableByWindowBackground:YES];
 }
+
+// ensureMainMenu 补齐 macOS 应用菜单栏的基础项：应用、文件、编辑、视图、窗口、帮助。
+// webview_go 不建菜单栏，无菜单时 Cmd+Q 等系统快捷键全部失效。
+void ensureMainMenu(void) {
+	NSMenu *bar = [[NSMenu alloc] init];
+	NSMenuItem *appItem = [bar addItemWithTitle:@"Coding" action:nil keyEquivalent:@""];
+	NSMenu *appMenu = [[NSMenu alloc] init];
+	[appMenu addItemWithTitle:@"关于 Coding" action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
+	[appMenu addItem:[NSMenuItem separatorItem]];
+	[appMenu addItemWithTitle:@"隐藏 Coding" action:@selector(hide:) keyEquivalent:@"h"];
+	id hideOthers = [appMenu addItemWithTitle:@"隐藏其他" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+	[hideOthers setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagOption];
+	[appMenu addItemWithTitle:@"显示全部" action:@selector(unhideAllApplications:) keyEquivalent:@""];
+	[appMenu addItem:[NSMenuItem separatorItem]];
+	[appMenu addItemWithTitle:@"退出 Coding" action:@selector(terminate:) keyEquivalent:@"q"];
+	[appItem setSubmenu:appMenu];
+
+	NSMenuItem *fileItem = [bar addItemWithTitle:@"文件" action:nil keyEquivalent:@""];
+	NSMenu *fileMenu = [[NSMenu alloc] init];
+	[fileMenu addItemWithTitle:@"新建窗口" action:@selector(newWindowForTab:) keyEquivalent:@"n"];
+	[fileMenu addItemWithTitle:@"关闭窗口" action:@selector(performClose:) keyEquivalent:@"w"];
+	[fileItem setSubmenu:fileMenu];
+
+	NSMenuItem *editItem = [bar addItemWithTitle:@"编辑" action:nil keyEquivalent:@""];
+	NSMenu *editMenu = [[NSMenu alloc] init];
+	[editMenu addItemWithTitle:@"撤销" action:@selector(undo:) keyEquivalent:@"z"];
+	[editMenu addItemWithTitle:@"重做" action:@selector(redo:) keyEquivalent:@"Z"];
+	[editMenu addItem:[NSMenuItem separatorItem]];
+	[editMenu addItemWithTitle:@"剪切" action:@selector(cut:) keyEquivalent:@"x"];
+	[editMenu addItemWithTitle:@"复制" action:@selector(copy:) keyEquivalent:@"c"];
+	[editMenu addItemWithTitle:@"粘贴" action:@selector(paste:) keyEquivalent:@"v"];
+	[editMenu addItemWithTitle:@"全选" action:@selector(selectAll:) keyEquivalent:@"a"];
+	[editItem setSubmenu:editMenu];
+
+	NSMenuItem *viewItem = [bar addItemWithTitle:@"视图" action:nil keyEquivalent:@""];
+	NSMenu *viewMenu = [[NSMenu alloc] init];
+	id toggleToolbar = [viewMenu addItemWithTitle:@"显示工具栏" action:@selector(toggleToolbarShown:) keyEquivalent:@"t"];
+	[toggleToolbar setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagOption];
+	id fullscreen = [viewMenu addItemWithTitle:@"切换全屏" action:@selector(toggleFullScreen:) keyEquivalent:@"f"];
+	[fullscreen setKeyEquivalentModifierMask:NSEventModifierFlagCommand | NSEventModifierFlagControl];
+	[viewItem setSubmenu:viewMenu];
+
+	NSMenuItem *windowItem = [bar addItemWithTitle:@"窗口" action:nil keyEquivalent:@""];
+	NSMenu *windowMenu = [[NSMenu alloc] init];
+	[windowMenu addItemWithTitle:@"最小化" action:@selector(performMiniaturize:) keyEquivalent:@"m"];
+	[windowMenu addItemWithTitle:@"缩放" action:@selector(performZoom:) keyEquivalent:@""];
+	[windowMenu addItem:[NSMenuItem separatorItem]];
+	[windowMenu addItemWithTitle:@"前置全部窗口" action:@selector(arrangeInFront:) keyEquivalent:@""];
+	[windowItem setSubmenu:windowMenu];
+
+	NSMenuItem *helpItem = [bar addItemWithTitle:@"帮助" action:nil keyEquivalent:@""];
+	NSMenu *helpMenu = [[NSMenu alloc] init];
+	[helpMenu addItemWithTitle:@"Coding 帮助" action:@selector(showHelp:) keyEquivalent:@""];
+	[helpItem setSubmenu:helpMenu];
+
+	[NSApp setMainMenu:bar];
+}
 */
 import "C"
 
@@ -23,6 +80,7 @@ import "unsafe"
 
 // apply 修改 NSWindow 样式为全尺寸内容视图（无边框观感），保留系统拖拽区。
 func apply(window interface{ Window() unsafe.Pointer }) {
+	C.ensureMainMenu()
 	if handle := window.Window(); handle != nil {
 		C.styleWindow(handle)
 	}
